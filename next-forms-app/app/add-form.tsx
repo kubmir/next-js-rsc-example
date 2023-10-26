9f38179ac977403b9e9518f5190d1a5a419da7c2
miroslavkubus <433645@muni.cz>
2023-10-26 09:39:02 +0200

    feat: make it work

--- next-forms-app/app/add-form.tsx
+++ next-forms-app/app/add-form.tsx
@@ -1,25 +1,28 @@
-'use client'
+"use client";
 
-import { experimental_useFormState as useFormState } from 'react-dom'
-import { useFormStatus } from 'react-dom'
-import { createTodo } from '@/app/actions'
+import {
+  experimental_useFormState as useFormState,
+  experimental_useFormStatus as useFormStatus,
+} from "react-dom";
+
+import { createTodo } from "@/app/actions";
 
 const initialState = {
   message: null,
-}
+};
 
 function SubmitButton() {
-  const { pending } = useFormStatus()
+  const { pending } = useFormStatus();
 
   return (
     <button type="submit" aria-disabled={pending}>
       Add
     </button>
-  )
+  );
 }
 
 export function AddForm() {
-  const [state, formAction] = useFormState(createTodo, initialState)
+  const [state, formAction] = useFormState(createTodo, initialState);
 
   return (
     <form action={formAction}>
@@ -30,5 +33,5 @@ export function AddForm() {
         {state?.message}
       </p>
     </form>
-  )
+  );
 }
--- next-forms-app/app/delete-form.tsx
+++ next-forms-app/app/delete-form.tsx
@@ -1,24 +1,27 @@
-'use client'
+"use client";
 
-import { useFormState, useFormStatus } from 'react-dom'
-import { deleteTodo } from '@/app/actions'
+import {
+  experimental_useFormState as useFormState,
+  experimental_useFormStatus as useFormStatus,
+} from "react-dom";
+import { deleteTodo } from "@/app/actions";
 
 const initialState = {
   message: null,
-}
+};
 
 function DeleteButton() {
-  const { pending } = useFormStatus()
+  const { pending } = useFormStatus();
 
   return (
     <button type="submit" aria-disabled={pending}>
       Delete
     </button>
-  )
+  );
 }
 
 export function DeleteForm({ id, todo }: { id: number; todo: string }) {
-  const [state, formAction] = useFormState(deleteTodo, initialState)
+  const [state, formAction] = useFormState(deleteTodo, initialState);
 
   return (
     <form action={formAction}>
@@ -29,5 +32,5 @@ export function DeleteForm({ id, todo }: { id: number; todo: string }) {
         {state?.message}
       </p>
     </form>
-  )
+  );
 }
